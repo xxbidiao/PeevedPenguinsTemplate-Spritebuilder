ade5a5ff81f49dd1fc3f3fcbf8d89c3832153b5f
LinLee <linlee@LinLees-MacBook-Air.local>
2015-02-05 23:36:24 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by LinLee on 2/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
