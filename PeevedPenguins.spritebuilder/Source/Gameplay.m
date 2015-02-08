78168b07681cf708d2f8463712f2ac7ae2f0852a
LinLee <linlee@LinLees-MacBook-Air.local>
2015-02-08 15:15:28 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -131,6 +131,7 @@
 -(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
 {
     CCLOG(@"Something collided with a seal!");
+    [self retry];
 }
 
 @end
