cca02ad62e0ae5937afe7fdf3b4f8467e94ea44e
LinLee <xxbidiao@gmail.com>
2015-03-10 21:50:52 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -32,7 +32,7 @@ static const float MIN_SPEED = 5.f;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
     // visualize physics bodies & joints
-    _physicsNode.debugDraw = TRUE;
+    _physicsNode.debugDraw = false;
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
